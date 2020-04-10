9d5f2bacb4910a79512784207ca6a8f5cb00726a
ale_allexandra99 <ale_allexandra99@yahoo.com>
2020-04-10 16:55:56 +0300

    application

---
+++ ConsoleApp4/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    public class Person
+    {
+        public string cnp;
+        public string last_name;
+        public string first_name;
+        public DateTime date_entry;
+        public DateTime date_output;
+        public void Intoducere ()
+        {
+            Person p = new Person();//crearea unui oviect
+            Console.Write("CNP: ");
+            p.cnp = Console.ReadLine();
+            Console.Write("nume:");
+            p.last_name = Console.ReadLine();
+            Console.Write("prenume:");
+            p.first_name = Console.ReadLine();
+            p.date_entry = DateTime.Now;
+            Console.WriteLine($"data de intrare {p.date_entry}");
+            p.date_output = DateTime.Now;
+            Console.WriteLine($"data de iesire {p.date_output}");
+            Program.list.Add(p); //introduc in lista obiectul de tip persoana
+        }
+        public override string ToString()
+        {
+           return string.Format($" {cnp} {last_name} {first_name} {date_entry} {date_output}");
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp4
+{
+    public class Program
+    {
+       public static List<Person> list = new List<Person>();//intializare lista si declarare de tip pesoana 
+        static void Main(string[] args)
+        {
+            
+            int x = -1;
+            do
+            {
+                Console.WriteLine("Daca doriti sa introduceti persoane introduceti 1 sau\n\t 0 daca doriti sa iesiti sau\n\t 2 daca doriti sa stergeti \n\t 3 editare persoana \n\t 4 afisarea persoanelor in functie de ore \n\t 5 afisarea persoanelor in functie de minute");
+                bool b;
+                do
+                {
+                    b = true;
+                    try
+                    {
+                        x = int.Parse(Console.ReadLine());
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Reintroduceti");
+                        b = false;
+
+
+                    }
+                } while (b == false);
+                if (x == 0)
+                    break;
+                if (x == 1)
+                {
+                    Person p = new Person();
+                    p.Intoducere();
+                }
+                if (x == 2)
+                {
+                    Console.WriteLine("Introduceti cnp");
+                    string cnp = Console.ReadLine();
+                    foreach (Person i in list)
+                    {
+                        if (i.cnp == cnp)
+                        {
+                            list.Remove(i);
+                            break;
+                        }
+                    }
+                }
+                if (x == 3)
+                {
+                    Console.WriteLine("Introduceti cnp");
+                    string cnp = Console.ReadLine();
+                    foreach (Person i in list)
+                    {
+                        if (i.cnp == cnp)
+                        {
+                            Console.WriteLine("nume: " + i.last_name);
+                            Console.Write("nume nou: ");
+                            string nume = Console.ReadLine();
+                            if (!(nume == " " || nume == ""))
+                                i.last_name = nume;
+                            Console.WriteLine("prenume: " + i.first_name);
+                            Console.Write("prenume nou: ");
+                            string prenume = Console.ReadLine();
+                            if (!(prenume == " " || prenume == ""))
+                                i.first_name = prenume;
+                            break;
+                        }
+                    }
+                }
+              /*  if (x == 4)
+                {
+                    string hour;
+                    Console.Write("ora: ");
+                    hour = Console.ReadLine();
+                    int y = Int32.Parse(hour);
+                    if ((y > 9))
+                    {
+                        foreach (Person i in list)
+                        {
+                            if ((i.date_entry[11] == hour[0]) && (i.date_entry[12] == hour[1]))
+                                Console.WriteLine(i);
+                        }
+                    }
+                    else
+                    {
+                        foreach (Person i in list)
+                        {
+                            if ((i.date_entry[12] == hour[0]))
+                                Console.WriteLine(i);
+                        }
+                    }
+                }
+                if(x==5)
+                {
+                    string minutes;
+                    Console.WriteLine("minute: ");
+                    minutes = Console.ReadLine();
+                    foreach(Person i in list)
+                    {
+                        if ((i.date_entry[14]==minutes[0]) &&(i.date_entry[15]==minutes[1]))
+                            Console.WriteLine(i);
+                    }*/
+                
+                if(x==5)
+                {
+                    int minutes;
+                    Console.Write("minute: ");
+                    minutes = int.Parse(Console.ReadLine());
+                    foreach(Person p in list)
+                    {
+                        if(p.date_entry.Minute==minutes)
+                            Console.WriteLine(p);
+                    }
+                }
+                } while (x != 0) ;
+                foreach (Person p in list)
+                    Console.WriteLine(p);
+            
+        }
+    }
+}
